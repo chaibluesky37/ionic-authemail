05b7f78f3992dd1d8c17792610073238318820cb
Chaiwat <36126822+chaibluesky37@users.noreply.github.com>
2018-02-19 15:28:00 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebase: {
-      apiKey: 'AIzaSyDwj2DN9HBan5nUkdGJihorBG2eMZW-Kvc',
-      authDomain: 'kmutnbpj3700.firebaseapp.com',
-      databaseURL: 'https://kmutnbpj3700.firebaseio.com',
-      projectId: 'kmutnbpj3700',
-      storageBucket: 'kmutnbpj3700.appspot.com',
-      messagingSenderId: '298972134522'
+      apiKey: 'xxxxxx',
+      authDomain: 'xxxxx',
+      databaseURL: 'xxxx',
+      projectId: 'xxxx',
+      storageBucket: 'xxxxx',
+      messagingSenderId: 'xxxxxx'
     }
   };
